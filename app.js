13c282193260cc7d0f13ad0a833f49c28389734a
Instagroam <68869355+Instagroam@users.noreply.github.com>
2020-07-27 23:48:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,15 @@
 
 ///Edit Under This
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyCg98MqMrnsBsNAhiDBt753SLydZXdePQg",
+  authDomain: "instagroam-c1389.firebaseapp.com",
+  databaseURL: "https://instagroam-c1389.firebaseio.com",
+  projectId: "instagroam-c1389",
+  storageBucket: "instagroam-c1389.appspot.com",
+  messagingSenderId: "469437337596",
+  appId: "1:469437337596:web:d6f2793d0624ca40502c79",
+  measurementId: "G-VCWQPWNVKB"
 };
 
      
